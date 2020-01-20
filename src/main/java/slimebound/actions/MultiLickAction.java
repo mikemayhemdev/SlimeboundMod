4426e0e51d61c155024b2540b1b65ee1ce9cfa14
Arek75 <arek75@gmail.com>
2020-01-20 06:40:37 -0700

    Comment out references to damageFlash and damageFlashFrames

--- src/main/java/slimebound/actions/MultiLickAction.java
+++ src/main/java/slimebound/actions/MultiLickAction.java
@@ -65,8 +65,8 @@ public class MultiLickAction extends AbstractGameAction {
                 } else {
 
                     if (this.target.currentHealth > 0) {
-                        this.target.damageFlash = true;
-                        this.target.damageFlashFrames = 4;
+                        // this.target.damageFlash = true;
+                        // this.target.damageFlashFrames = 4;
                         AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, this.attackEffect));
                        // this.info.applyPowers(this.info.owner, this.target);
                        // this.target.damage(this.info);
